6e844a64d821d1b02c82729bb8ff142582aa8031
GISELE MARA DOS ANJOS <gisele_anjos@estudante.sesisenai.org.br>
2024-03-28 17:06:31 -0300

    variable name changed

--- src/Main.java
+++ src/Main.java
@@ -6,13 +6,13 @@ public class Main {
        int numeroInteiro = 9;
        float numeroReal = 2.22f;
        char letra = 'g';
-       boolean terminou = false;
+       boolean logicc = false;
        String joke = "Here goes a joke...";
         //sout atalho para System.out.println
        System.out.println(joke);
 
 
-       System.out.printf("An integer number '%d', a float number '%.1f', a char letter '%C' and a '%b' boolean enter a bar.", numeroInteiro, numeroReal, letra, terminou);
+       System.out.printf("An integer number '%d', a float number '%.1f', a char letter '%C' and a '%b' boolean enter a bar.", numeroInteiro, numeroReal, letra, logicc);
 
 
     }
